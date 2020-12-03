883a7334c5be62f24803a8e9fe91e37a2da1fc0e
Abele Aynekulu <aynekuab@dukes.jmu.edu>
2020-12-03 02:35:17 -0500

    done with log in page

--- src/pages/welcome/components/SignIn.js
+++ src/pages/welcome/components/SignIn.js
@@ -1,10 +1,69 @@
+import "./SignIn.css";
+import Avatar from "@material-ui/core/Avatar";
+import PersonIcon from "@material-ui/icons/Person";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Button from "@material-ui/core/Button";
 
-import './SignIn.css';
-export default function SignIn(props){
-    return(
-        <div className = "SignIn-main-container">
-            <h1 className = "welome-text">Welcome to Genois</h1>
+export default function SignIn(props) {
+  return (
+    <div className="SignIn-main-container">
+      <div className="welcome-text-container">
+        <h1 className="welome-text">Welcome to Genois</h1>
+      </div>
+      <div className="avatar-container">
+        <Avatar className="avatar">
+          <PersonIcon />
+        </Avatar>
+      </div>
+      <div className="userinfo-container">
+        <form className="form-classes" noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="Username"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+        </form>
 
+        <div className="smt-button-container">
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            size="medium"
+          >
+            Sign In
+          </Button>
         </div>
-    );
+        <Link
+          component="button"
+          variant="body2"
+          
+        >
+           Don't Have any account? Join us
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+function exportUserInfo(){
+
 }
