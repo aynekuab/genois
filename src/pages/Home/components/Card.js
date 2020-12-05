6adc7bb1eb6ac4f2cee513ec059719110f524556
Abele Aynekulu <aynekuab@dukes.jmu.edu>
2020-12-05 06:20:53 -0500

    same

--- src/pages/Home/components/Card.js
+++ src/pages/Home/components/Card.js
@@ -27,7 +27,7 @@ export default function Cards(props) {
         <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
           This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
+         , if you like.
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
